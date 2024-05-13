9d82b4320ecdec25e114f64de2cfa62c602eedd1
NicolassBacher <nico.8291@gmail.com>
2024-05-13 01:07:28 -0300

    filtro por mes

--- JS/main.js
+++ JS/main.js
@@ -1,79 +1,105 @@
-
-
-
 const meses = document.querySelector('#mes')
-const paquetes = document.querySelectorAll('.paq_box')
-const noDisponible = document.querySelector('.paq_falla')
-
+const contenido = document.querySelector('.paq_contenido')
 
 const hoy = new Date()
 const anioActual = new Date().getFullYear()
 const mesActual = new Date().getMonth() + 1
 const diaActual = new Date().getDate()
 let viajesVigentes = []
-let viajesProx6 = []
 let viajess = {}
 let mostrar = []
-
+let semana = {
+    '0': 'Dom',
+    '1': 'Lun',
+    '2': 'Mar',
+    '3': 'Mie',
+    '4': 'Jue',
+    '5': 'Vie',
+    '6': 'Sab'
+}
 
 function validador(data) {
     viajesVigentes = []
     Object.entries(data.viajes).forEach(([key, value]) => {
-        if (new Date(key) > hoy) {              //Filtrar tambien para que sea menores a un anio, osea, por ejemplo de mayo del 24 a abril de 25
+        let salida = new Date(value.salida)
+        if (salida > hoy && (salida.getFullYear() == anioActual || (salida.getFullYear() == anioActual + 1 && salida.getMonth() <= mesActual))) {
             viajesVigentes.push(key)
         }
     })
     viajesVigentes.sort()
-    console.log(viajesVigentes)
 }
 
 function arrayFiltro(viajes) {
     mostrar = []
     if (mes.value == 'prox') {
-
-        mostrar = [viajesVigentes[0], viajesVigentes[1], viajesVigentes[2], viajesVigentes[3], viajesVigentes[4], viajesVigentes[5]]
-        console.log(mostrar, 'proximos viajes')
-    } else {
-        viajesVigentes.forEach((viajeVigente) => {
-            console.log(new Date(viajeVigente).getMonth() + 1, 'mes', mes.value)
-            if (new Date(viajeVigente).getMonth() + 1 == mes.value) {
-                console.log('entre')
-                mostrar.push(viajeVigente)
-
+        mostrar = [viajesVigentes[0], viajesVigentes[1], viajesVigentes[2], viajesVigentes[3], viajesVigentes[4], viajesVigentes[5], viajesVigentes[6], viajesVigentes[7]]
+        mostrar.forEach((fecha, i) => {
+            console.log(mostrar, i)
+            if (fecha == undefined) {
+                mostrar.splice(i)
             }
         })
-        //if(viajesVigentes.getMonth())
-    } console.log(mostrar)
-
-}
-
-function apagarTodo() {
-    paquetes.forEach(paquete => paquete.style.display = 'none')
+    } else {
+        viajesVigentes.forEach((viajeVigente) => {
+            if (new Date(viajes.viajes[viajeVigente].salida).getMonth() + 1 == mes.value) {
+                mostrar.push(viajeVigente)
+            }
+        })
+    }
 }
 
 function armado(armar, datos) {
-    console.log(armar)
+    contenido.innerHTML = ''
     if (armar.length == 0 || datos == null) {
-        console.log('entre')
-        noDisponible.innerHTML = 'Lo sentimos, no disponemos de viajes para la fecha seleccionada, intente con otra fecha :)'
-        noDisponible.style.display = 'block'
-
+        contenido.innerHTML = '<h3 class="paq_falla">Lo sentimos, no disponemos de viajes para la fecha seleccionada, intente con otra fecha :)</h3>'
     }
     else {
-        noDisponible.style.display = 'none'
-    }
-    //console.log(armar)
-    armar.forEach((fecha, i) => {
-        if (datos.viajes[fecha] != undefined) {
-            //console.log(datos.viajes[fecha])
-            //console.log(i, paquetes[i], 'aca' + datos.viajes[fecha].titulo)
-            paquetes[i].childNodes[3].childNodes[1].innerHTML = datos.viajes[fecha].titulo
-            paquetes[i].childNodes[3].childNodes[3].innerHTML = datos.viajes[fecha].tiempo
-            paquetes[i].childNodes[3].childNodes[5].innerHTML = datos.viajes[fecha].descripcion
-            paquetes[i].childNodes[3].childNodes[9].childNodes[1].innerHTML = "$" + datos.viajes[fecha].precio
-            paquetes[i].style.display = 'flex'
+        const box = `<div class="paq_box">
+        <img class="paq_portada" src="" alt=""/>
+            <div class="paq_data">
+                <h3></h3>
+                <p class="paq_fecha"></p>
+                <p class="paq_fecha"></p>
+                <p class="paq_descripcion"></p>
+                <hr class="paq_linea" />
+                <div class="paq_precio">
+                    <h2></h2>
+                    <button class="paq_btn_info">+ info</button>
+                </div>
+            </div>
+        </div > `
+        for (var i = 0; i < armar.length; i++) {
+            contenido.innerHTML += box
         }
-    })
+        let paquetes = document.querySelectorAll('.paq_box')
+
+        armar.forEach((id, i) => {
+            let dato = datos.viajes[id]
+            if (dato.salida != undefined) {
+                let paq = paquetes[i].childNodes
+                let salida = new Date(dato.salida)
+                let regreso = new Date(dato.regreso)
+                let tiempoDeViaje = ` ${dato.dias} dias ${dato.noches} noches | ${semana[salida.getDay()]} ${salida.getDate()}/${regreso.getMonth() + 1
+                    } al ${semana[regreso.getDay()]} ${regreso.getDate()} /${regreso.getMonth() + 1}`
+
+                let transporte = (a) => {
+                    if (a.toLowerCase() == 'avion') {
+                        return '<img src="../images/avion.png" alt="avion"/>'
+                    } else {
+                        return '<img src="../images/autobus.png" alt="avion"/>'
+                    }
+
+                }
+                paq[1].src = dato.img
+                paq[1].alt = dato.alt
+                paq[3].childNodes[1].innerHTML = dato.titulo
+                paq[3].childNodes[3].innerHTML = '<img src="../images/calendario.png" alt="calendario"/>' + ' &nbsp' + tiempoDeViaje
+                paq[3].childNodes[5].innerHTML = transporte(dato.transporte) + ' &nbsp ' + dato.transporte
+                paq[3].childNodes[7].innerHTML = dato.descripcion
+                paq[3].childNodes[11].childNodes[1].innerHTML = "$" + dato.precio
+            }
+        })
+    }
 }
 
 
@@ -83,17 +109,14 @@ async function cargarJSON() {
         const viajes = await response.json()
         viajess = viajes
         validador(viajes)
-        //console.log('JSON', e.target.value)
         arrayFiltro(viajes)
-        apagarTodo()
         armado(mostrar, viajes)
     }
     catch (error) {
         console.log('ERROR AL CARGAR API', error)
-        noDisponible.innerHTML = 'Lo sentimos, ha ocurrido un error al cargar los datos, vuelva a intentarlo mas tarde :)'
-        noDisponible.style.display = 'block'
+        contenido.innerHTML = ''
+        contenido.innerHTML = '<h3 class="paq_falla">Lo sentimos, ha ocurrido un error al cargar los datos, vuelva a intentarlo mas tarde :)</h3>'
     }
 }
-
 meses.addEventListener('change', cargarJSON)
 cargarJSON()
